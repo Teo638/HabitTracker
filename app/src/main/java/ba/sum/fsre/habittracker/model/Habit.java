30633310b2c4d3cb1f4df041285feda41dab9e83
franpavlovi <fljubuski@gmail.com>
2026-01-04 22:06:04 +0100

    habit model&repo

---
+++ app/src/main/java/ba/sum/fsre/habittracker/model/Habit.java
@@ -0,0 +1,83 @@
+package ba.sum.fsre.habittracker.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Habit {
+
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("user_id")
+    private String userId;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("frequency")
+    private String frequency;
+
+    @SerializedName("created_at")
+    private String createdAt;
+
+
+    public Habit() {}
+
+    public Habit(String userId, String title, String description, String frequency) {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.frequency = frequency;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
+
