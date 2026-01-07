2b77910ed9da47f0be6d8144c66c8a8a4a5db2b2
matijaudovicic <118848184+matijaudovicic@users.noreply.github.com>
2026-01-07 22:55:17 +0100

    Dodala ChallengeRepository klasu za upravljanje podacima o izazovima koristeci SessionManager za autorizaciju.

---
+++ app/src/main/java/ba/sum/fsre/habittracker/repo/ChallengeRepository.java
@@ -0,0 +1,26 @@
+package ba.sum.fsre.habittracker.repo;
+
+import android.content.Context;
+import java.util.List;
+import ba.sum.fsre.habittracker.api.SupabaseClient;
+import ba.sum.fsre.habittracker.api.SupabaseDataApi;
+import ba.sum.fsre.habittracker.utils.SessionManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import ba.sum.fsre.habittracker.model.Challenge;
+
+public class ChallengeRepository {
+    private final SupabaseDataApi api;
+    private final SessionManager sessionManager;
+
+    public ChallengeRepository(Context context) {
+        this.api = SupabaseClient.getClient().create(SupabaseDataApi.class);
+        this.sessionManager = new SessionManager(context);
+    }
+
+    public void getAllChallenges(Callback<List<Challenge>> callback) {
+        if (sessionManager.getToken() == null) return;
+        String token = "Bearer " + sessionManager.getToken();
+        api.getAllChallenges(token, "anon", "*").enqueue(callback);
+    }
+}
